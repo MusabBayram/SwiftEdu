d00fd35dff3ed0e517fed416f532940b62607597
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2022-12-14 20:03:41 +0300

    TableView app

---
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Musab Bahadır Bayram on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource 	{
+    
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.textLabel?.text = "test"
+        var content = cell.defaultContentConfiguration()
+        content.text = "TableViewFirstText"
+        content.secondaryText = "Secondary Text"
+        cell.contentConfiguration = content
+        return cell
+    }
+
+
+}
+
