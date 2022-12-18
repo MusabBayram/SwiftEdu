b0849937896b5b7743061825d3b246242178c254
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2022-12-18 12:10:36 +0300

    some changes

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -10,24 +10,41 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource 	{
     
     
-
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonhange")
+        landmarkNames.append("Taj Mahal")
+        
+        landmarkImages.append(UIImage(named: "colosseum")!)
+        landmarkImages.append(UIImage(named: "greatwall")!)
+        landmarkImages.append(UIImage(named: "kremlin")!)
+        landmarkImages.append(UIImage(named: "stonehange")!)
+        landmarkImages.append(UIImage(named: "tajmahal")!)
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         //cell.textLabel?.text = "test"
         var content = cell.defaultContentConfiguration()
-        content.text = "TableViewFirstText"
-        content.secondaryText = "Secondary Text"
+        content.text = landmarkNames[indexPath.row]
+        //content.secondaryText = "Secondary Text"
         cell.contentConfiguration = content
         return cell
     }
