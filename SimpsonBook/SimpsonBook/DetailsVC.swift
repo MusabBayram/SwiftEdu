37c83041d91dec929d4f0d8a9d516eaf09055216
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2022-12-24 10:51:08 +0300

    some changes

---
+++ SimpsonBook/SimpsonBook/DetailsVC.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Musab Bahadır Bayram on 24.12.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    @IBOutlet weak var JobNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    
+
+}
--- SimpsonBook/SimpsonBook/ViewController.swift
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    var simpsonNames = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        TableView.delegate = self
+        TableView.dataSource = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return simpsonNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        
+        cell.contentConfiguration = content
+        return cell
+    }
 
 }
 
