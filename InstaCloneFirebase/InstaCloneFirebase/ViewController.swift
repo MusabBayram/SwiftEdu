ccdf760f61424003dabe5f80585aea3c73f26e7b
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-03-27 18:44:02 +0300

    Update ViewController.swift

--- InstaCloneFirebase/InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
@@ -22,6 +23,28 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }
+                else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+        else {
+            makeAlert(title: "Error", message: "Username/Password?")
+        }
+    }
+    
+    func makeAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 }
