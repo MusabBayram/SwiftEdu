419e79608486e04a43b0f2836a41690f3c2f3832
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-03-30 17:46:16 +0300

    some upload

--- InstaCloneFirebase/InstaCloneFirebase/SettingsViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SettingsViewController: UIViewController {
 
@@ -17,7 +18,13 @@ class SettingsViewController: UIViewController {
     
 
     @IBAction func logoutClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toViewController", sender: nil)
+        do {
+            try Auth.auth().signOut()
+            performSegue(withIdentifier: "toViewController", sender: nil)
+        }
+        catch {
+            print("Error")
+        }
     }
 
 }
--- InstaCloneFirebase/InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/ViewController.swift
@@ -15,11 +15,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func signInClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }
+                else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+        else {
+            makeAlert(title: "Error", message: "Username/Password?")
+        }
+        
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
