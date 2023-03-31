f78c1dbcd30780b86e8430cc17daab28e864ada9
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-03-31 16:43:03 +0300

    Update UploadViewController.swift

--- InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
@@ -35,6 +36,32 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UI
     }
     
     @IBAction func actionButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data) { metadata, error in
+                
+                if error != nil {
+                    print(error?.localizedDescription)
+                }
+                else {
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
     }
     
 
