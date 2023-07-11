4f6471473168040570bf8966529c6948a09a05de
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-11 17:41:08 +0300

    some changes

---
+++ AngryBirdClone/AngryBirdClone/GameScene.swift
@@ -0,0 +1,52 @@
+//
+//  GameScene.swift
+//  AngryBirdClone
+//
+//  Created by Musab Bahadır Bayram on 7.07.2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+       
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
