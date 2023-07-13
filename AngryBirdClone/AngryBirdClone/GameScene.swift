bf7b8da93d38a3dbc53f4edcd01ea12a0631fd13
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-13 14:40:09 +0300

    Update GameScene.swift

--- AngryBirdClone/AngryBirdClone/GameScene.swift
+++ AngryBirdClone/AngryBirdClone/GameScene.swift
@@ -8,7 +8,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var bird = SKSpriteNode()
     var box1 = SKSpriteNode()
@@ -21,11 +21,17 @@ class GameScene: SKScene {
     
     var originalPos : CGPoint?
     
+    enum ColliderType : UInt32 {
+        case Bird = 1
+        case Box = 2
+    }
+    
     override func didMove(to view: SKView) {
         
         //Physics Body
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         self.scene?.scaleMode = .aspectFit
+        self.physicsWorld.contactDelegate = self
         
         //Bird
         bird = childNode(withName: "bird") as! SKSpriteNode
@@ -37,6 +43,11 @@ class GameScene: SKScene {
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.15
         originalPos = bird.position
+        
+        bird.physicsBody?.contactTestBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.categoryBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         
         //Box
         
@@ -50,12 +61,16 @@ class GameScene: SKScene {
         box1.physicsBody?.allowsRotation = true
         box1.physicsBody?.mass = 0.4
         
+        box1.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box2 = childNode(withName: "box2") as! SKSpriteNode
         box2.physicsBody = SKPhysicsBody(rectangleOf: size)
         box2.physicsBody?.isDynamic = true
         box2.physicsBody?.affectedByGravity = true
         box2.physicsBody?.allowsRotation = true
         box2.physicsBody?.mass = 0.4
+        
+        box2.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
         
         box3 = childNode(withName: "box3") as! SKSpriteNode
         box3.physicsBody = SKPhysicsBody(rectangleOf: size)
@@ -64,6 +79,8 @@ class GameScene: SKScene {
         box3.physicsBody?.allowsRotation = true
         box3.physicsBody?.mass = 0.4
         
+        box3.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box4 = childNode(withName: "box4") as! SKSpriteNode
         box4.physicsBody = SKPhysicsBody(rectangleOf: size)
         box4.physicsBody?.isDynamic = true
@@ -71,12 +88,25 @@ class GameScene: SKScene {
         box4.physicsBody?.allowsRotation = true
         box4.physicsBody?.mass = 0.4
         
+        box4.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box5 = childNode(withName: "box5") as! SKSpriteNode
         box5.physicsBody = SKPhysicsBody(rectangleOf: size)
         box5.physicsBody?.isDynamic = true
         box5.physicsBody?.affectedByGravity = true
         box5.physicsBody?.allowsRotation = true
         box5.physicsBody?.mass = 0.4
+        
+        box5.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.collisionBitMask == ColliderType.Bird.rawValue || contact.bodyB.collisionBitMask == ColliderType.Bird.rawValue {
+            
+            print("dafds")
+        }
         
     }
     
@@ -191,5 +221,21 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if let birdPhysicsBody = bird.physicsBody {
+            
+            if birdPhysicsBody.velocity.dx <= 0.1 && birdPhysicsBody.velocity.dy <= 0.1 &&
+                birdPhysicsBody.angularVelocity <= 0.1 && gameStarted == true {
+                
+                bird.physicsBody?.affectedByGravity = false
+                bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                bird.physicsBody?.angularVelocity = 0
+                bird.zPosition = 1
+                bird.position = originalPos!
+                gameStarted = false
+                
+            }
+            
+        }
     }
 }
