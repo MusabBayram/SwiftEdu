de8db105ca7ed0e7652bdb3434277b2c578ea6cf
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-24 15:12:47 +0300

    Protocol

---
+++ ProtocolSwiftProject/ProtocolSwiftProject/main.swift
@@ -0,0 +1,55 @@
+//
+//  main.swift
+//  ProtocolSwiftProject
+//
+//  Created by Musab Bahadır Bayram on 24.07.2023.
+//
+
+import Foundation
+
+protocol Running {
+    func myRun()
+}
+
+class Animal {
+    func test() {
+        print("test")
+    }
+}
+
+class Dog : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let barley = Dog()
+barley.myRun()
+
+class Cat : Animal, Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let cat = Cat()
+cat.test()
+cat.myRun()
+
+class Turtle : Animal {
+    
+}
+
+let leonardo = Turtle()
+
+
+struct Bird : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let tweety = Bird()
+tweety.myRun()
+
+
