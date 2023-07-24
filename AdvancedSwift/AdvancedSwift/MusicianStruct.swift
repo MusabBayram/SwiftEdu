66ea01c5f5eb89cb8cb286cce2843c68bcbd1853
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-24 15:21:44 +0300

    Update MusicianStruct.swift

--- AdvancedSwift/AdvancedSwift/MusicianStruct.swift
+++ AdvancedSwift/AdvancedSwift/MusicianStruct.swift
@@ -1,10 +1,4 @@
-//
-//  MusicianStruct.swift
-//  AdvancedSwiftProject
-//
-//  Created by Atil Samancioglu on 11.08.2019.
-//  Copyright © 2019 Atil Samancioglu. All rights reserved.
-//
+
 
 import Foundation
 
