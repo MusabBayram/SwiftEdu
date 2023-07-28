7977f323cd550be314be2c0eac4f88f3bc294074
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-28 11:53:17 +0300

    MVVM Example

---
+++ CryptoCrazy/CryptoCrazy/Model/CryptoCurrency.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazy
+//
+//  Created by Musab Bahadır Bayram on 28.07.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency {
+    
+    let currency : String
+    let price: String
+}
---
+++ CryptoCrazy/CryptoCrazy/View/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CryptoCrazy
+//
+//  Created by Musab Bahadır Bayram on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell") as! CryptoTableViewCell
+        
+        return cell
+    }
+
+
+}
+
