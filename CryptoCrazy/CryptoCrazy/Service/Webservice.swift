9c0af06180da2d9aedcfdbb0cf01c58fd2b03aca
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-07-28 17:19:20 +0300

    Create Webservice.swift

---
+++ CryptoCrazy/CryptoCrazy/Service/Webservice.swift
@@ -0,0 +1,32 @@
+//
+//  Webservice.swift
+//  CryptoCrazy
+//
+//  Created by Musab Bahadır Bayram on 28.07.2023.
+//
+
+import Foundation
+
+class Webservice {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+            else if let data = data {
+                
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+            
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+                    print(cryptoList)
+                }
+            }
+        }.resume()
+        
+        
+    }
+}
