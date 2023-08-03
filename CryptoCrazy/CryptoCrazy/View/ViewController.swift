b93f0ad17c9f92b95534b3fe06c47d8676fc41e8
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-03 23:35:55 +0300

    Update ViewController.swift

--- CryptoCrazy/CryptoCrazy/View/ViewController.swift
+++ CryptoCrazy/CryptoCrazy/View/ViewController.swift
@@ -12,27 +12,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     private var cryptoListViewModel: CryptoListViewModel!
     
+    let cryptoVM = CryptoViewModel(cryptoCurrency: <#CryptoCurrency#>)
+    
+    var cryptoList = [Crypto]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
         
+        getData()
+    
+    }
+    
+    func getData() {
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         
         Webservice().downloadCurrencies(url: url) { (cryptos) in
             if let cryptos = cryptos {
                 
+                self.cryptoListViewModel = CryptoListViewModel(cryptoCurrencyList: cryptos)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             }
         }
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.cryptoListViewModel == nil ? 0 : self.cryptoListViewModel.numberOfRowInSection()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell") as! CryptoTableViewCell
+        
+        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
+        
+        cell.priceText.text = cryptoViewModel.price
+        cell.currencyText.text = cryptoViewModel.name
         
         return cell
     }
