3cbcf11861a876b2495b616c54b15bc026c85056
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-12 10:58:41 +0300

    Update CryptoViewModel.swift

--- CryptoCrazy/CryptoCrazy/ViewModel/CryptoViewModel.swift
+++ CryptoCrazy/CryptoCrazy/ViewModel/CryptoViewModel.swift
@@ -6,31 +6,37 @@
 // 
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 
-struct CryptoListViewModel {
+class CryptoViewModel {
     
-    let cryptoCurrencyList: [Crypto]
     
-    func numberOfRowInSection() -> Int {
-        return self.cryptoCurrencyList.count
-    }
+    public let cryptos : PublishSubject<[Crypto]> = PublishSubject()
+    public let loading: PublishSubject<Bool> = PublishSubject()
+    public let error : PublishSubject<String> = PublishSubject()
     
-    func cryptoAtIndex(_ index: Int) -> CryptoViewModel {
-        let crypto = self.cryptoCurrencyList[index]
-        return CryptoViewModel(cryptoCurrency: crypto)
-    }
-}
-
-struct CryptoViewModel {
     
-    let cryptoCurrency: Crypto
-    
-    var name: String {
-        return self.cryptoCurrency.currency
-    }
-    
-    var price: String {
-        return self.cryptoCurrency.price
+    public func requestData(){
+        
+        self.loading.onNext(true)
+        Webservice().downloadCurrencies(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!) { cryptoResult in
+            self.loading.onNext(false)
+            switch cryptoResult {
+            case .success(let cryptos):
+                print(cryptos)
+                self.cryptos.onNext(cryptos)
+            case .failure(let failure):
+                switch failure {
+                case .parsingEror:
+                    self.error.onNext("Cannot parse your data")
+                case .serverError:
+                    self.error.onNext("Cannot get your data at all")
+                }
+            }
+        }
+        
+        
     }
 }
