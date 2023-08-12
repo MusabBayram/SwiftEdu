681042da74ad54fec5a1f7a89269642e3a1ac1ce
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-12 11:07:21 +0300

    Update CryptoTableViewCell.swift

--- CryptoCrazy/CryptoCrazy/View/CryptoTableViewCell.swift
+++ CryptoCrazy/CryptoCrazy/View/CryptoTableViewCell.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class CryptoTableViewCell: UITableViewCell {
-    @IBOutlet weak var currencyText: UILabel!
-    @IBOutlet weak var priceText: UILabel!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,5 +25,14 @@ class CryptoTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+ 
+    public var item: Crypto! {
+           didSet {
+               self.nameLabel.text = item.currency
+               self.priceLabel.text = item.price
+
+           }
+       }
+    
 
 }
