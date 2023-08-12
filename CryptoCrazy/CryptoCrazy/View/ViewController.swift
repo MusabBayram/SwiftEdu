6144393f336f146ae62ff8f3226344905d063601
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-12 11:07:23 +0300

    Update ViewController.swift

--- CryptoCrazy/CryptoCrazy/View/ViewController.swift
+++ CryptoCrazy/CryptoCrazy/View/ViewController.swift
@@ -6,58 +6,88 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class ViewController: UIViewController, UITableViewDelegate {
+   
+    let disposeBag = DisposeBag()
+    let cryptoVM = CryptoViewModel()
+    //var cryptoList = [Crypto]()
+
     @IBOutlet weak var tableView: UITableView!
-    private var cryptoListViewModel: CryptoListViewModel!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
     
-    let cryptoVM = CryptoViewModel(cryptoCurrency: <#CryptoCurrency#>)
-    
-    var cryptoList = [Crypto]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        getData()
-    
+        //tableView.delegate = self
+        //tableView.dataSource = self
+        view.backgroundColor = .black
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        setupBindings()
+        cryptoVM.requestData()
     }
     
-    func getData() {
-        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
+    private func setupBindings() {
+            
+        //do not forget to select hides when stopped property of indicator in storyboard
+            cryptoVM.loading
+                .bind(to: self.indicatorView.rx.isAnimating)
+                .disposed(by: disposeBag)
+            
+                        
+            cryptoVM
+                .error
+                .observe(on: MainScheduler.asyncInstance)
+                .subscribe(onNext: { failure in
+                    print(failure)
+                })
+                .disposed(by: disposeBag)
+            
+        //get the cryptos like this by observing or bind the data directly to cell like below
+        //in order to bind, we delete datasourcedelegate from vc, and bind the view table like below
+        //we have to create a custom cell
         
-        Webservice().downloadCurrencies(url: url) { (cryptos) in
-            if let cryptos = cryptos {
-                
-                self.cryptoListViewModel = CryptoListViewModel(cryptoCurrencyList: cryptos)
-                
-                DispatchQueue.main.async {
+        /*
+            cryptoVM
+                .cryptos
+                .observe(on: MainScheduler.instance)
+                .subscribe(onNext: { crypto in
+                    self.cryptoList = crypto
                     self.tableView.reloadData()
-                }
-                
-            }
+                })
+                .disposed(by: disposeBag)
+        */
+           
+        cryptoVM.cryptos
+            .observe(on: MainScheduler.asyncInstance)
+            .bind(to: tableView.rx.items(cellIdentifier: "cryptoTableViewCellID", cellType: CryptoTableViewCell.self)) { (row,item,cell) in
+            cell.item = item
+        }.disposed(by: disposeBag)
+        
+        tableView.rx.modelSelected(Crypto.self).subscribe(onNext: { item in
+            print("SelectedItem: \(item.currency)")
+            }).disposed(by: disposeBag)
         }
-    }
     
-    
+  /*
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.cryptoListViewModel == nil ? 0 : self.cryptoListViewModel.numberOfRowInSection()
+        return cryptoList.count
     }
-    
+ 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell") as! CryptoTableViewCell
-        
-        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
-        
-        cell.priceText.text = cryptoViewModel.price
-        cell.currencyText.text = cryptoViewModel.name
-        
-        return cell
-    }
+            let cell = UITableViewCell()
+            var content = cell.defaultContentConfiguration()
+            content.text = cryptoList[indexPath.row].currency
+            content.secondaryText = cryptoList[indexPath.row].price
+            cell.contentConfiguration = content
+            return cell
 
+    }
+     */
+     
+     
 
 }
-
