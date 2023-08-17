9682775b5d64d752d55911d8c6073617a900d142
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-17 18:05:25 +0300

    some changes

---
+++ SnapchatClone/SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Musab Bahadır Bayram on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
