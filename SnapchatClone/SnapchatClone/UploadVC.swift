9e8998275f643caac7cdc7279194366d5326630b
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-29 14:06:13 +0300

    Update UploadVC.swift

--- SnapchatClone/SnapchatClone/UploadVC.swift
+++ SnapchatClone/SnapchatClone/UploadVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseFirestore
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -33,6 +35,57 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data) { metadata, error in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }
+                else {
+                    
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            
+                            let fireStore = Firestore.firestore()
+                            
+                            let snapDictionary = ["imageUrl" : imageUrl!, "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                            
+                            fireStore.collection("Snaps").addDocument(data: snapDictionary) { (error) in
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+            
+            
+            
+    }
+    
+    
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 }
