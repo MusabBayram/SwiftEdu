64b41bb745307218ffab0b486089647a54e28df1
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-31 14:26:52 +0300

    Update FeedVC.swift

--- SnapchatClone/SnapchatClone/FeedVC.swift
+++ SnapchatClone/SnapchatClone/FeedVC.swift
@@ -13,6 +13,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     let fireStoreDatabase = Firestore.firestore()
+    var snapArray = [Snap]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,7 +28,40 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func getSnapsFromFirebase() {
-        
+        fireStoreDatabase.collection("Snaps").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
+            if error != nil {
+                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error!")
+            }
+            else {
+                if snapshot?.isEmpty == false && snapshot != nil {
+                    self.snapArray.removeAll(keepingCapacity: false)
+                    for document in snapshot!.documents {
+                        
+                        let documentId = document.documentID
+                        
+                        if let username = document.get("snapOwner") as? String {
+                            if let imageUrlArray = document.get("imageUrlArray") as? [String] {
+                                if let date = document.get("date") as? Timestamp {
+                                    
+                                    if let difference = Calendar.current.dateComponents([.hour], from: date.dateValue(), to: Date()).hour {
+                                        if difference >= 24 {
+                                            self.fireStoreDatabase.collection("Snaps").document(documentId).delete{ (error) in
+                                                
+                                            }
+                                        }
+                                        // TIMELEFT -> SNAPVC
+                                    }
+                                    
+                                    let snap = Snap(username: username, imageUrlArray: imageUrlArray, date: date.dateValue())
+                                    self.snapArray.append(snap)
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     func getUserInfo(){
@@ -57,12 +92,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return snapArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.feedUserNameLabel.text = "test"
+        cell.feedUserNameLabel.text = snapArray[indexPath.row].username
         return cell
     }
 
