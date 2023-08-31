5697fb28c20e9c943e3b55ffb30b50152133bf45
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-08-31 14:26:49 +0300

    Create Snap.swift

---
+++ SnapchatClone/SnapchatClone/Model/Snap.swift
@@ -0,0 +1,14 @@
+//
+//  Snap.swift
+//  SnapchatClone
+//
+//  Created by Musab Bahadır Bayram on 31.08.2023.
+//
+
+import Foundation
+
+struct Snap {
+    var username : String
+    var imageUrlArray : [String]
+    var date : Date
+}
