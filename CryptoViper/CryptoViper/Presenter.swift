7e33f2224a81dca90083ad270016f1b6fbfc70f7
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-09-04 17:37:42 +0300

    Update Presenter.swift

--- CryptoViper/CryptoViper/Presenter.swift
+++ CryptoViper/CryptoViper/Presenter.swift
@@ -11,3 +11,35 @@ import Foundation
 //talks to -> interactor, router, view
 //class, protocol
 
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFailed
+}
+
+protocol AnyPresenter {
+    
+    var router : AnyRouter? {get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView {get set}
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var router: AnyRouter?
+    
+    var interactor: AnyInteractor?
+    
+    var view: AnyView
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let crptos):
+            print("dasddfs")
+        case .failure(let error):
+            print("dfsafdsf")
+        }
+    }
+    
+    
+}
