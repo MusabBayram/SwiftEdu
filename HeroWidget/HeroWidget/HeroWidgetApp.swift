53779bc830259349eedbb29a7c0df085cc8066cb
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-09-11 10:55:08 +0300

    widget project

---
+++ HeroWidget/HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Musab Bahadır Bayram on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeroWidget/HeroWidget/Superhero.swift
@@ -0,0 +1,24 @@
+//
+//  Superhero.swift
+//  HeroWidget
+//
+//  Created by Musab Bahadır Bayram on 11.09.2023.
+//
+
+import Foundation
+
+struct Superhero : Identifiable, Codable {
+    
+    let image : String
+    let name: String
+    let realName : String
+    
+    var id : String {image}
+    
+}
+
+let superman = Superhero(image: "ironman", name: "Ironman", realName: "Tony Stark")
+
+let batman = Superhero(image: "batman", name: "Batman", realName: "Bruce Wayne")
+
+let deadpool = Superhero(image: "deadpool", name: "DeadPool", realName: "Wade Wilson")
