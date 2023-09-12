35bf643a05f39d808b69c2a0fa0531a7187c5efa
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-09-12 11:25:26 +0300

    circular image view

---
+++ HeroWidget/HeroWidget/CircularImageView.swift
@@ -0,0 +1,27 @@
+//
+//  CircularImageView.swift
+//  HeroWidget
+//
+//  Created by Musab Bahadır Bayram on 12.09.2023.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    
+    var image : Image
+    
+    var body: some View {
+        image.resizable().aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle()
+                .stroke(Color.red, lineWidth: 10))
+            .shadow(radius: 20)
+    }
+}
+
+struct CircularImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularImageView(image: Image("deadpool"))
+    }
+}
