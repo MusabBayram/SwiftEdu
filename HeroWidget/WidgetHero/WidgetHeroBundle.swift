b6c65b9121b1391f128ca12adfd54a3461955688
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-09-18 17:17:02 +0300

    new extantion

---
+++ HeroWidget/WidgetHero/WidgetHeroBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHeroBundle.swift
+//  WidgetHero
+//
+//  Created by Musab Bahadır Bayram on 18.09.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetHeroBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetHero()
+        WidgetHeroLiveActivity()
+    }
+}
