10bf2f88fc5323983c58dee1dc36ccdc8bccf304
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-10-19 11:27:18 +0300

    Update JokesData.swift

--- JokesAppSwiftUI/JokesAppSwiftUI/JokesData.swift
+++ JokesAppSwiftUI/JokesAppSwiftUI/JokesData.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 // MARK: - Welcome
-struct Welcome: Codable {
+struct Welcome: Identifiable, Codable {
+    let id = UUID()
     let type: String
     let value: [Value]
 }
 
 // MARK: - Value
-struct Value: Codable {
+struct Value: Identifiable, Codable {
     let id: Int
     let joke: String
     let categories: [String]
